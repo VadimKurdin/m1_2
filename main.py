52593b65564cc13fa2cae630135e328d2bd1a2b9
VadimKurdin <vkurdin23@gmail.com>
2025-03-29 19:21:52 +0300

    add_...

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 """)
 
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])...
 @bot.message_handler(commands=['info'])
 def echo_message(message):
     bot.reply_to(message, message.text)
